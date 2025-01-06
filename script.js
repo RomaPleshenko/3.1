003aa0b167b25dcf54d0b0d4f5ba6dacec677739
Roma Pleshenko <romaple28@gmail.com>
2025-01-06 23:23:54 +0200

    3.1

---
+++ script.js
@@ -0,0 +1,18 @@
+let num = 15;
+console.log(typeof num);
+let string = "Hello world!";
+console.log(typeof string);
+let nul = null;
+console.log(typeof nul);
+let bull = true;
+console.log(typeof bull);
+let bigint = 12345n;
+console.log(typeof bigint);
+let undefined = undefined;
+console.log(typeof undef);
+let object = {};
+console.log(typeof object);
+let symbol = Symbol();
+console.log(typeof symbol);
+let array = [];
+console.log(typeof array)
